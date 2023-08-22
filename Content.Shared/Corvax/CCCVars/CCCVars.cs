3deb534565faf39037f9ae2549ff2dd0d8d35de1
Timofey <t1m1yep@gmail.com>
2023-08-22 15:44:38 +0300

    Update CCVars

--- Content.Shared/Corvax/CCCVars/CCCVars.cs
+++ Content.Shared/Corvax/CCCVars/CCCVars.cs
@@ -59,19 +59,19 @@ public sealed class CCCVars
     /// URL of the TTS server API.
     /// </summary>
     public static readonly CVarDef<bool> TTSEnabled =
-        CVarDef.Create("tts.enabled", false, CVar.SERVERONLY | CVar.ARCHIVE);
+        CVarDef.Create("tts.enabled", true, CVar.SERVERONLY | CVar.ARCHIVE);
 
     /// <summary>
     /// URL of the TTS server API.
     /// </summary>
     public static readonly CVarDef<string> TTSApiUrl =
-        CVarDef.Create("tts.api_url", "", CVar.SERVERONLY | CVar.ARCHIVE);
+        CVarDef.Create("tts.api_url", string.Empty, CVar.SERVERONLY | CVar.ARCHIVE);
 
     /// <summary>
     /// Auth token of the TTS server API.
     /// </summary>
     public static readonly CVarDef<string> TTSApiToken =
-        CVarDef.Create("tts.api_token", "", CVar.SERVERONLY | CVar.CONFIDENTIAL);
+        CVarDef.Create("tts.api_token", string.Empty, CVar.SERVERONLY | CVar.CONFIDENTIAL);
 
     /// <summary>
     /// Amount of seconds before timeout for API
